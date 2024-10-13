d2a10c7e3d9938e880617798cef055e6af7f23d3
ervin-sungkono <ervinsung@gmail.com>
2024-10-13 12:54:33 +0700

    enable transition on theme change

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,6 @@ export default function RootLayout({
             <ThemeProvider 
               attribute="class" 
               enableSystem={false}
-              disableTransitionOnChange
             >
               {children}
             </ThemeProvider>
